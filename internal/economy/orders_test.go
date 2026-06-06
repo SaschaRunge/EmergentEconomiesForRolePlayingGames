2c5e5459ed158757a8c8cd7de3314320c75be70d
SaschaRunge <sascharunge89@yahoo.de>
2026-06-06 19:08:08 +0200

    expand unit tests to purchases, add description for each

--- internal/economy/orders_test.go
+++ internal/economy/orders_test.go
@@ -7,12 +7,13 @@ import (
 
 func TestDetermineSaleQuantity(t *testing.T) {
 	cases := []struct {
-		expected int
-		input    CommodityState
+		description string
+		expected    int
+		input       CommodityState
 	}{
-		// nothing to sell
 		{
-			expected: 0,
+			description: "nothing to sell",
+			expected:    0,
 			input: CommodityState{
 				availableInventory: 0,
 				excessInventory:    0,
@@ -20,9 +21,9 @@ func TestDetermineSaleQuantity(t *testing.T) {
 				priceBelief:        rpgMath.PriceRange{Min: 5, Max: 15},
 			},
 		},
-		// historical mean lower than price belief
 		{
-			expected: 0,
+			description: "historical mean lower than price belief",
+			expected:    0,
 			input: CommodityState{
 				availableInventory: 0,
 				excessInventory:    10,
@@ -30,9 +31,9 @@ func TestDetermineSaleQuantity(t *testing.T) {
 				priceBelief:        rpgMath.PriceRange{Min: 15, Max: 30},
 			},
 		},
-		// historical mean higher than price belief
 		{
-			expected: 10,
+			description: "historical mean higher than price belief",
+			expected:    10,
 			input: CommodityState{
 				availableInventory: 0,
 				excessInventory:    10,
@@ -40,9 +41,9 @@ func TestDetermineSaleQuantity(t *testing.T) {
 				priceBelief:        rpgMath.PriceRange{Min: 15, Max: 30},
 			},
 		},
-		// historical mean is at 20% of price belief range
 		{
-			expected: 2,
+			description: "historical mean is at 20% of price belief range",
+			expected:    2,
 			input: CommodityState{
 				availableInventory: 0,
 				excessInventory:    10,
@@ -50,9 +51,9 @@ func TestDetermineSaleQuantity(t *testing.T) {
 				priceBelief:        rpgMath.PriceRange{Min: 10, Max: 20},
 			},
 		},
-		// historical mean is at 80% of price belief range
 		{
-			expected: 8,
+			description: "historical mean is at 80% of price belief range",
+			expected:    8,
 			input: CommodityState{
 				availableInventory: 0,
 				excessInventory:    10,
@@ -65,7 +66,73 @@ func TestDetermineSaleQuantity(t *testing.T) {
 	for _, c := range cases {
 		actual := DetermineSaleQuantity(c.input)
 		if actual != c.expected {
-			t.Errorf("expected: %d, actual: %d", c.expected, actual)
+			t.Errorf("case: %q, expected: %d, actual: %d", c.description, c.expected, actual)
+		}
+	}
+}
+
+func TestDeterminePurchaseQuantity(t *testing.T) {
+	cases := []struct {
+		description string
+		expected    int
+		input       CommodityState
+	}{
+		{
+			description: "no inventory space",
+			expected:    0,
+			input: CommodityState{
+				availableInventory: 0,
+				excessInventory:    0,
+				historicalMean:     10,
+				priceBelief:        rpgMath.PriceRange{Min: 5, Max: 15},
+			},
+		},
+		{
+			description: "historical mean lower than price belief",
+			expected:    10,
+			input: CommodityState{
+				availableInventory: 10,
+				excessInventory:    0,
+				historicalMean:     10,
+				priceBelief:        rpgMath.PriceRange{Min: 15, Max: 30},
+			},
+		},
+		{
+			description: "historical mean higher than price belief",
+			expected:    0,
+			input: CommodityState{
+				availableInventory: 10,
+				excessInventory:    0,
+				historicalMean:     40,
+				priceBelief:        rpgMath.PriceRange{Min: 15, Max: 30},
+			},
+		},
+		{
+			description: "historical mean is at 20% of price belief range",
+			expected:    8,
+			input: CommodityState{
+				availableInventory: 10,
+				excessInventory:    0,
+				historicalMean:     12,
+				priceBelief:        rpgMath.PriceRange{Min: 10, Max: 20},
+			},
+		},
+		{
+			description: "historical mean is at 80% of price belief range",
+			expected:    2,
+			input: CommodityState{
+				availableInventory: 10,
+				excessInventory:    0,
+				historicalMean:     18,
+				priceBelief:        rpgMath.PriceRange{Min: 10, Max: 20},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		actual := DeterminePurchaseQuantity(c.input)
+		if actual != c.expected {
+			t.Errorf("case: %q, expected: %d, actual: %d", c.description, c.expected, actual)
 		}
 	}
 }
